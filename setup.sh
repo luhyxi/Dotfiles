043dbcb180e0538441856b7d4456de506c15ea63
luhyxi <luhyxi@gmail.com>
2025-07-14 20:08:38 -0300

    reorganizing some stuff + redone the install script

--- setup.sh
+++ setup.sh
@@ -1,2 +1,21 @@
-#! /bin/bash
-ln -sf "$(pwd)/bashrc" "$HOME/.bashrc" && mkdir -p "$HOME/.config" && ln -sf "$(pwd)/config/kitty" "$HOME/.config/kitty" && ln -sf "$(pwd)/config/nvim" "$HOME/.config/nvim" && ln -sf "$(pwd)/scripts" "$HOME/scripts"
+##!/bin/bash
+
+# Create necessary directories if they don't exist
+mkdir -p ~/.config/kitty
+mkdir -p ~/.config/nvim
+
+# Link bash configuration
+ln -sf ~/Dotfiles/bashrc ~/.bashrc
+
+# Link kitty configuration
+ln -sf ~/Dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
+ln -sf ~/Dotfiles/kitty/current-theme.conf ~/.config/kitty/current-theme.conf
+
+# Link nvim configuration (entire directory)
+rm -rf ~/.config/nvim
+ln -sf ~/Dotfiles/nvim ~/.config/nvim
+
+# Verify links were created
+echo "Dotfiles linked successfully!"
+echo "Created links:"
+ls -la ~/.bashrc ~/.config/kitty/ ~/.config/nvim 
