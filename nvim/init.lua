832875e199b954f35b4cdf97cad0a8f0606d7400
luhyxi <luhyxi@gmail.com>
2025-07-19 22:32:24 -0300

    added automplete

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,7 @@
 require("config.lazy")
 require("config.keymaps")
 require("config.vim_cmd")
-
+require("config.completions")
 require("lazy").setup("plugins")
-
 
 vim.cmd.colorscheme "catppuccin"
--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -16,9 +16,21 @@ return {
         },
         config = function()
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.omnisharp.setup({})
-            lspconfig.ts_ls.setup({})
+
+            -- Get capabilities from nvim-cmp
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+            -- Setup LSP servers with nvim-cmp capabilities
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.omnisharp.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.ts_ls.setup({
+                capabilities = capabilities,
+            })
+
         end
     },
 }
