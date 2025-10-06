fe9aa65b3202a821f1ae3a30c7364a35bf47d261
luhyxi <luhyxi@gmail.com>
2025-10-06 19:09:40 -0300

    feat: added tmux navigator to my neovim conf

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,4 +1,5 @@
 local map = vim.keymap.set
+local nvim_tmux_nav = require("nvim-tmux-navigation")
 
 local builtin = require("telescope.builtin")
 
@@ -7,7 +8,7 @@ map("n", "<leader>f", builtin.find_files, { desc = "Find files in the working di
 map("n", "<leader>g", builtin.live_grep, { desc = "Live GREPs the working directory" })
 map("n", "<leader>b", builtin.buffers, { desc = "Opens GREP for recent buffers" })
 map("n", "<leader>q", ":quit<CR>", { desc = "Quits buffer" })
-map("n", "<leader>w", ":write<CR>",{ desc = "Writes to buffer" })
+map("n", "<leader>w", ":write<CR>", { desc = "Writes to buffer" })
 
 -- Tab stuff
 map("n", "<leader>tt", ":tabnew<CR>", { desc = "New tab" })
@@ -30,8 +31,23 @@ map("n", "K", vim.lsp.buf.hover, { desc = "Display info over function" })
 map("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic error messages" })
 map("n", "gd", vim.lsp.buf.definition, {})
 map("n", "gD", "<cmd>tab split | lua vim.lsp.buf.definition()<CR>", {})
-map("n", "grr", vim.lsp.buf.references, {desc = "Lists all the references to the symbol under the cursor in the quickfix window"})
+
+map(
+	"n",
+	"grr",
+	vim.lsp.buf.references,
+	{ desc = "Lists all the references to the symbol under the cursor in the quickfix window" }
+)
+
 map("n", "d", '"_d', { noremap = true, silent = true, desc = "Delete without copying" })
 map("v", "d", '"_d', { noremap = true, silent = true, desc = "Delete without copying" })
 map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Show avalible code actions" })
 map("n", "<leader>=", "gg=G", { desc = "Format entire file with = operator" })
+
+-- tmux keymaps
+map("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+map("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+map("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+map("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+map("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+map("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
