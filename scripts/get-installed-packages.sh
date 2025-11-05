6f32d0872fc324578b8d75457742384889c1c643
luhyxi <luhyxi@gmail.com>
2025-11-05 11:46:31 -0300

    feat: removed the weird scripts

--- scripts/get-installed-packages.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-yay -Q > pkglist.txt
--- scripts/install_pkgs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ln -sf "$(pwd)/bashrc" "$HOME/.bashrc" && mkdir -p "$HOME/.config" && ln -sf "$(pwd)/config/kitty" "$HOME/.config/kitty" && ln -sf "$(pwd)/config/nvim" "$HOME/.config/nvim" && ln -sf "$(pwd)/scripts" "$HOME/scripts"
