1ba3095212e50228ee2c54483715236547c1cfcc
luhyxi <luhyxi@gmail.com>
2025-11-23 15:31:24 -0300

    feat: new stuff for setting up stuff

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 # Create necessary directories if they don't exist
 mkdir -p ~/.config/kitty
 mkdir -p ~/.config/nvim
+mkdir -p ~/.config/tmux/tmux.conf
 
 # Link bash configuration
 ln -sf ~/Dotfiles/bashrc ~/.bashrc
@@ -12,7 +13,7 @@ ln -sf ~/Dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
 ln -sf ~/Dotfiles/kitty/current-theme.conf ~/.config/kitty/current-theme.conf
 
 # Link tmux configuration
-ln -sf ~/Dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/Dotfiles/tmux.conf ~/,config/tmux/tmux.conf
 
 # Link nvim configuration (entire directory)
 rm -rf ~/.config/nvim
