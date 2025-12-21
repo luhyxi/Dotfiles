2491b24fc4838887e8aa2e934045685356ad1cbc
luhyxi <luhyxi@gmail.com>
2025-12-21 17:04:42 -0300

    feat: added a script to install kube

---
+++ install-kube-stuff.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Installing Kind
+
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.31.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
